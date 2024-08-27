be69000e2a78e0abeb9cd21888eaa18b10d48317
cashm <cashm@CASH-COM>
2024-08-27 10:34:26 -0500

    Testing the Git Commit Add project files.

---
+++ test_fall24_cash.cpp
@@ -0,0 +1,13 @@
+// test_fall24_cash.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "This is a test to see if it'll commit to github.";
+}
+
